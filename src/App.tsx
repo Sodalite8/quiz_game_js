5fa56f45fe7497af16104415d120c68050a202aa
Sodalite8 <murasakisub8@gmail.com>
2024-08-23 18:52:29 +0900

    replace the css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { SCREENS } from "./constants";
-import './App.css';
+import './css/App.css';
 import Title from "./game_ja/title";
 import Option from "./game_ja/option"
 import Menu0 from './game_ja/menu0';
