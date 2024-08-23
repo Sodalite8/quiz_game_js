2eb3ed35ff9370e20f04bd2d461a988ce1ffdac4
Sodalite8 <murasakisub8@gmail.com>
2024-08-23 21:22:44 +0900

    revise UI

--- src/game_ja/Title.tsx
+++ src/game_ja/Title.tsx
@@ -14,7 +14,7 @@ function Title(props: Props) {
             国旗クイズゲーム
         </h1>
         <button onClick={() => props.setScreen(SCREENS.MENU0)}>スタート</button>
-        <button onClick={() => props.setScreen(SCREENS.LIST0)}>図鑑</button>
+        <button onClick={() => props.setScreen(SCREENS.LIST0)}>国旗リスト</button>
         <button onClick={() => props.setScreen(SCREENS.OPTION)}>設定</button>
         </>
     );
