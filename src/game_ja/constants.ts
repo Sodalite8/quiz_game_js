9e753f5cee8f896eaa89ad36ef8104ef1869eac9
Sodalite8 <murasakisub8@gmail.com>
2024-08-24 20:10:16 +0900

    move constants.ts to game_ja

--- src/game_ja/constants.ts
+++ src/game_ja/constants.ts
@@ -11,23 +11,27 @@ export interface Options {
     music_volume: number;
     effect_volume: number;
     animation: boolean;
-}
-
+};
 export const INITIAL_OPTIONS: Options = {
     music_volume: 100,
     effect_volume: 100,
     animation: true
-}
+};
 
+export interface Options_Const {
+    max_volume: number;
+    min_volume: number;
+};
+export const OPTIONS_CONST: Options_Const = {
+    max_volume: 100,
+    min_volume: 0
+};
 
 export interface Quiz_Options {
     difficulty: string;
     problems_num: number;
-}
-
+};
 export const INITIAL_QUIZ_OPTIONS: Quiz_Options = {
     difficulty: "easy",
     problems_num: 10
-}
-
-
+};
