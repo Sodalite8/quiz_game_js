1b90e07b7ec6be106c594a0a17bae69c92602ced
Sodalite8 <murasakisub8@gmail.com>
2024-09-08 23:50:40 +0900

    tailwind設定変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.html", "./src/game/**/*.tsx"],
+    content: ["./public/index.html", "./src/index.tsx", "./src/game/**/*.tsx"],
     theme: {
         extend: {
             spacing: {
