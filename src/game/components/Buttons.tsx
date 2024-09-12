d90883d60f826154a6e6bdca6885a0355424eb36
Sodalite8 <murasakisub8@gmail.com>
2024-09-13 00:56:23 +0900

    ButtonとRangesのpropsの型定義を変更 ボタン効果音追加 useSoundをadd 音声を読み込む部分に不具合有り

--- src/game/components/Buttons.tsx
+++ src/game/components/Buttons.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-
-
-interface Props {
-    text?: string | undefined;
-    name?: string | undefined;
-    click?: React.MouseEventHandler<HTMLButtonElement> | undefined;
-}
+import { play_normal_btn } from "../scripts/loadSound";
 
 
 const insertLineBreak = (str: string | undefined) => {
-    if(str === undefined) {
+    if (str === undefined) {
         return null;
     }
 
@@ -24,13 +18,28 @@ const insertLineBreak = (str: string | undefined) => {
 };
 
 
+interface Props {
+    text?: string;
+    name?: string;
+    click?: () => void;
+}
+
+
 export function SmallButton(props: Props) {
+    const clickSmallButton = () => {
+        play_normal_btn();
+        if(props.click !== undefined) {
+            props.click();
+        }
+    }
+
+
     return (
         <>
             <button className='m-2 w-small-btn-w cursor-pointer 
                 rounded-lg border-2 border-orange-300 bg-orange-400 
-                p-1 text-lg font-bold text-white' 
-                onClick={props.click}>
+                p-1 text-lg font-bold text-white'
+                onClick={clickSmallButton}>
                 {insertLineBreak(props.text)}
             </button>
         </>
@@ -39,12 +48,20 @@ export function SmallButton(props: Props) {
 
 
 export function MediumButton(props: Props) {
+    const clickMediumButton = () => {
+        play_normal_btn();
+        if(props.click !== undefined) {
+            props.click();
+        }
+    }
+
+
     return (
         <>
             <button className='m-4 block w-medium-btn-w cursor-pointer 
                 rounded-lg border-2 border-orange-300 bg-orange-400 
-                p-2 text-xl font-bold text-white' 
-                onClick={props.click}>
+                p-2 text-xl font-bold text-white'
+                onClick={clickMediumButton}>
                 {insertLineBreak(props.text)}
             </button>
         </>
@@ -53,12 +70,21 @@ export function MediumButton(props: Props) {
 
 
 export function LargeButton(props: Props) {
+    const clickLargeButton = () => {
+        console.log("音がなります")
+        play_normal_btn();
+        if(props.click !== undefined) {
+            props.click();
+        }
+    }
+
+
     return (
         <>
             <button className='m-8 block w-large-btn-w cursor-pointer 
                 rounded-lg border-2 border-orange-300 bg-orange-400 
-                p-4 text-2xl font-bold text-white' 
-                onClick={props.click}>
+                p-4 text-2xl font-bold text-white'
+                onClick={clickLargeButton}>
                 {insertLineBreak(props.text)}
             </button>
         </>
@@ -66,13 +92,20 @@ export function LargeButton(props: Props) {
 }
 
 
-export function AnswerButton(props: Props) {
+interface Props1 {
+    text?: string;
+    name?: string;
+    click?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+
+export function AnswerButton(props: Props1) {
     return (
         <>
             <button className="m-2 block h-answer-btn-h w-answer-btn-w 
                 cursor-pointer rounded-lg border-2 border-orange-300 
-                bg-orange-400 p-2 text-xl font-bold text-white" 
-                name={props.name} 
+                bg-orange-400 p-2 text-xl font-bold text-white"
+                name={props.name}
                 onClick={props.click}>
                 {insertLineBreak(props.text)}
             </button>
---
+++ src/game/scripts/loadSound.ts
@@ -0,0 +1,5 @@
+import useSound from "use-sound";
+
+
+export const [play_normal_btn, handle_normal_button] 
+    = useSound("../audio/normal_btn");
