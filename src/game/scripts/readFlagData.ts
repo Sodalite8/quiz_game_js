a527189f81000a0e2d3e65c90d08b3c6de12935a
Sodalite8 <murasakisub8@gmail.com>
2024-09-23 16:23:22 +0900

    分類名追加 分類名のリストを読み込むように変更

--- src/game/scripts/readFlagData.ts
+++ src/game/scripts/readFlagData.ts
@@ -1,5 +1,6 @@
 import { FLAG_DATA_CONST, FlagData } from "../../_constants/constants";
 import flag_data_list_json from "../texts/flags.json";
+import categories_data_json from "../texts/categories.json";
 
 
 const readFlagDataByKey = (key: "level" | "category", size: number): FlagData[][] => {
@@ -18,3 +19,8 @@ const readFlagDataByKey = (key: "level" | "category", size: number): FlagData[][
 export const FLAG_DATA_LIST: FlagData[] = flag_data_list_json;
 export const FLAG_DATA_LIST_BY_LEVEL: FlagData[][] = readFlagDataByKey("level", FLAG_DATA_CONST.level_num);
 export const FLAG_DATA_LIST_BY_CATEGORY: FlagData[][] = readFlagDataByKey("category", FLAG_DATA_CONST.category_num);
+
+
+export const CATEGORIES_LIST: string[] = categories_data_json;
+export const CATEGORIES_NUM: number = categories_data_json.length;
+
