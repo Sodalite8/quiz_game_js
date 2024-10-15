f99dbf68efda559e3c021429633725afe110f44f
Sodalite8 <08sodium.lithium@gmail.com>
2024-10-15 11:18:55 +0900

    警告メッセージを追加

--- src/game/App.tsx
+++ src/game/App.tsx
@@ -16,10 +16,42 @@ function App() {
     const [screen, setScreen] = React.useState<number>(SCREENS.TITLE);
     const [options, setOptions] = React.useState<Options>(INITIAL_OPTIONS);
     const [quiz_options, setQuizOptions] = React.useState<QuizOptions>(INITIAL_QUIZ_OPTIONS);
+    const [show_message, setShowMessage] = React.useState<boolean>(true);
+
+
+    React.useEffect(() => {
+        const startTimer = setTimeout(() => {
+            setShowMessage(false);
+        }, 2000);
+
+        return () => clearTimeout(startTimer);
+    }, []);
 
 
     // screenによって映し出す画面を変更
     const renderScreen = () => {
+        // ページに入ってから数秒間表示するメッセージ
+        if (show_message) {
+            return (
+                <>
+                    <div className='relative flex size-full flex-col items-center'>
+                        <h1 className='relative top-1/4 text-center text-6xl font-bold'>
+                            注意！
+                        </h1>
+
+                        <h2 className='relative top-1/3 text-center text-4xl font-bold'>
+                            本ゲームは音が出ます<br />
+                            周囲の環境と音量に注意してお楽しみください
+                        </h2>
+                    </div>
+
+                </>
+            );
+        }
+
+        
+
+
         switch (screen) {
             case SCREENS.TITLE:     // タイトル
                 return (
